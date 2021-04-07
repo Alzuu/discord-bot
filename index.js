9f2387365dc0c8f85005e12ce127a3f10f694654
Allan Li <allan0499@gmail.com>
2021-04-07 14:02:07 +0300

    Linting

--- index.js
+++ index.js
@@ -1,73 +1,55 @@
-const { Client, Collection, RichEmbed } = require('discord.js');
-const { config } = require('dotenv');
+const { Client, Collection } = require('discord.js');
+
+require('dotenv').config();
+
 const client = new Client();
 
-const ytdl = require('ytdl-core');
-
-// Colletions
+// Collections
 client.commands = new Collection();
 client.aliases = new Collection();
 
-config({
-  path: __dirname + '/.env'
-});
-
-// Runs command-loader
-['command'].forEach(handler => {
-  require(`./handlers/${handler}`)(client);
-});
+// Initialize command handler
+require('./commands')(client);
 
 client.once('ready', () => {
   console.log('Ready!');
 
   client.user.setPresence({
     status: 'online',
-    game: {
-      name: 'Allan tinkering me',
-      type: 'WATCHING'
-    }
+    activity: {
+      name: 'Allu doing tiktok dances',
+      type: 'WATCHING',
+    },
   });
 });
 
-client.on('message', message => {
-  const prefix = '§';
+client.on('message', async (message) => {
+  const prefix = '!';
+  if (!message.content.startsWith(prefix) && message.content.includes('free')) return message.channel.send('allu thats toxic');
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  if (message.content.toLowerCase().includes('lmao') && !message.author.bot) {
-    message.channel.send('lmao');
-  } else if (!message.content.startsWith(prefix) || message.author.bot) {
-    return;
-  } else if (!message.guild) {
-    return;
-  } else {
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
-    // If no command with said name, find aliases. If no aliases either, exit early. If there is, get the command and call its run-method while passing in the client, your message and args variables as the method arguments. In case something goes wrong, log the error and report back.
+  const command = client.commands.get(commandName);
 
-    const command =
-      client.commands.get(commandName) ||
-      client.commands.find(
-        cmd => cmd.aliases && cmd.aliases.includes(commandName)
-      );
+  if (!command) return;
 
-    if (!command) return;
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
 
-    if (command.args && !args.length) {
-      let reply = `You didn't provide any arguments, ${message.author}!`;
-
-      if (command.usage) {
-        reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-      }
-
-      return message.channel.send(reply);
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
     }
 
-    try {
-      command.run(client, message, args);
-    } catch (error) {
-      console.error(error);
-      message.reply('There was an error trying to run the command!');
-    }
+    message.channel.send(reply);
+  }
+
+  try {
+    command.run(client, message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to run the command!');
   }
 });
 
